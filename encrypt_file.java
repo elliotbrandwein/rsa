5ebe4de70d539eed1eada84fa49e27be397cd2f7
Elliot Brandwein <elliotbrandwein1@gmail.com>
2017-10-16 11:33:36 -0400

    added in args requirement

--- encrypt_file.java
+++ encrypt_file.java
@@ -66,26 +66,24 @@ public class encrypt_file {
   }
   public static void main(String[] args) throws IOException
   {
-    // String key_file = "";
-    // String message_file = "";
-    // if(args.length == 2)
-    // {
-    //   key_file = args[0];
-    //   message_file = args[1];
-    // }
-    // else
-    // {
-    //   System.out.println("Incorrect number of args.\nPlease give the key file, then the message file.\nTerminating program");
-    //   System.exit(0);
-    // }
+    String key_file = "";
+    String message_file = "";
+    if(args.length == 2)
+    {
+      key_file = args[0];
+      message_file = args[1];
+    }
+    else
+    {
+      System.out.println("Incorrect number of args.\nPlease give the key file, then the message file.\nTerminating program");
+      System.exit(0);
+    }
 
-    // String[] keys = get_keys(key_file);
-    // BigInteger letter = new BigInteger("0");
-    // BigInteger E = new BigInteger(keys[4]);
-    // BigInteger N = new BigInteger(keys[5]);
-     
-    String message_file = "face.jpg";
-
+    String[] keys = get_keys(key_file);
+    BigInteger letter = new BigInteger("0");
+    BigInteger E = new BigInteger(keys[4]);
+    BigInteger N = new BigInteger(keys[5]);
+  
     FileOutputStream out = null;
     List<Byte> message; 
     try
